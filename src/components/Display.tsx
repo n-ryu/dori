5ae7ff9359c3c759eb1c3410355b4479b8dc0817
n-ryu <ababc100564@gmail.com>
2025-05-04 17:35:58 +0900

    feat: implement Helix class to generate helix curve from original curve

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,42 @@
 import { Canvas } from "@react-three/fiber";
 import ICAL from "ical.js";
-
+import { LineCurve3, Vector3 } from "three";
+import { Line, TrackballControls } from "@react-three/drei";
+import { useMemo } from "react";
+import { Helix } from "../utils/Helix";
 interface Props {
   events: ICAL.Event[];
 }
 
 export const Display = ({ events }: Props) => {
-  return <Canvas />;
+  const axis = useMemo(
+    () => new LineCurve3(new Vector3(0, 0, 0), new Vector3(0, 0, 10)),
+    []
+  );
+
+  const helix = useMemo(() => new Helix(axis, 2, 12), [axis]);
+
+  const dHelix = useMemo(() => new Helix(helix, 0.1, 356), [helix]);
+
+  return (
+    <div style={{ width: 960, height: 560 }}>
+      <Canvas>
+        <Line
+          color="#FF0000"
+          lineWidth={1}
+          points={new Array(10000)
+            .fill(0)
+            .map((_, i) => dHelix.getPoint(i / 10000))}
+        />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial />
+        </mesh>
+        <color attach="background" args={["#EEEEFF"]} />
+        <ambientLight intensity={0.1} color="#EEFFEE" />
+        <directionalLight position={[0, 0, 5]} color="red" />
+        <TrackballControls />
+      </Canvas>
+    </div>
+  );
 };
---
+++ src/utils/Helix.ts
@@ -0,0 +1,48 @@
+import { Curve, Vector3 } from "three";
+
+export class Helix extends Curve<Vector3> {
+  constructor(
+    private originalCurve: Curve<Vector3>,
+    private radius: number,
+    private numberOfRotations: number
+  ) {
+    super();
+  }
+
+  getPoint(t: number) {
+    const point = this.originalCurve.getPoint(t);
+    const tangent = this.originalCurve.getTangent(t);
+
+    const delta = 0.0001;
+    const t1 = t > delta ? t - delta : 0;
+    const t2 = 1 - t > delta ? t + delta : 1;
+
+    const tangent1 = this.originalCurve.getTangent(t1);
+    const tangent2 = this.originalCurve.getTangent(t2);
+    const rawNormal = new Vector3()
+      .subVectors(tangent2, tangent1)
+      .projectOnPlane(tangent)
+      .normalize();
+
+    /* TODO: when tangent1 and tangent2 is identical, rawNormal would return [0,0,0]. 
+    For this case, it needs to return a fallback value. Currently, I set it to unit vector toward X axis, 
+    but would be best if it can return an arbitrary unit vector perpendicular to tanget */
+    const normal = rawNormal.length() === 0 ? new Vector3(1, 0, 0) : rawNormal;
+
+    const up = new Vector3().crossVectors(normal, tangent);
+
+    const result = new Vector3().addVectors(
+      point,
+      new Vector3().addVectors(
+        normal.multiplyScalar(
+          this.radius * Math.cos(2 * Math.PI * t * this.numberOfRotations)
+        ),
+        up.multiplyScalar(
+          this.radius * Math.sin(2 * Math.PI * t * this.numberOfRotations)
+        )
+      )
+    );
+
+    return result;
+  }
+}
