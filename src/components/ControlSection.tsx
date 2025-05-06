9a3519b27a33aa02736dfbaaa0f51d1e542f1960
n-ryu <ababc100564@gmail.com>
2025-05-06 15:14:27 +0900

    feat: show the center date on the control panel

--- src/components/ControlSection.tsx
+++ src/components/ControlSection.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import "./ControlSection.css";
 import dayjs from "dayjs";
+import { useControls } from "leva";
+import { datesControl } from "../controls/controls";
 
 export const ControlSection = () => {
   const [now, setNow] = useState<Date>(new Date());
+
+  const [{ today }] = useControls(...datesControl);
 
   useEffect(() => {
     const interval = setInterval(() => setNow(new Date()), 1000);
@@ -18,7 +22,7 @@ export const ControlSection = () => {
       </div>
       <div className="row">
         <label className="label">Look At</label>
-        <span>{dayjs(now).format("YYYY-MM-DD HH:mm:ss")}</span>
+        <span>{dayjs(today).format("YYYY-MM-DD HH:mm:ss")}</span>
       </div>
     </div>
   );
